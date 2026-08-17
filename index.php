ae0ac3f5d91c05989cff7667317f389e08ff7d2d
dannnbsit <lucbandanilo_bsit@plmun.edu.ph>
2026-08-17 19:31:11 +0800

    adding if statement ...

--- index.php
+++ index.php
@@ -1,4 +1,37 @@
-<?
+<?php
+
+// If Statement
+$firstName = "Danilo";
+
+if ($firstName === "Danilo") {
+    echo "Correct";
+    echo "<br>";
+}
+
+// If else Statement
+$userName = "guest";
+$userPass = 1234;
+
+if ($userName === "guest" && $userPass === 1234) {
+    echo "Correct";
+} else {
+    echo "Incorrect";
+}
+
+
+// If... elseif... else Statement
+$age = 69;
+
+if ($age <= 17) {
+    echo "Minor";
+} else if ($age <= 59) {
+    echo "Adult";
+} else {
+    echo "Senior";
+}
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -9,6 +42,6 @@
     <title>Assignment 1</title>
 </head>
 <body>
-    <h1>add file</h1>
+
 </body>
 </html>
