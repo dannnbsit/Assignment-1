7c0384678ba56c55328949d5bf774e47305a0997
dannnbsit <lucbandanilo_bsit@plmun.edu.ph>
2026-08-17 18:24:40 +0800

    add html

--- index.php
+++ index.php
@@ -4,3 +4,15 @@
     echo "hiiiii";
     echo "hiiiii";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignment 1</title>
+</head>
+<body>
+    <h1>add file</h1>
+</body>
+</html>
